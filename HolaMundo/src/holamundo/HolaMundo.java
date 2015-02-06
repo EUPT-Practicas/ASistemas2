d69c54fbadfc9476ec76daaa1384e1be1c512b32
jnavarro90 <jnavarroa90@gmail.com>
2015-02-06 19:25:25 +0100

    Prueba v1.1
    Modificación para probar git

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package holamundo;
 
 /**
  *
- * @author Ricardo
+ * @author Ricardo y Javier
+ * @version 1.1
  */
 public class HolaMundo {
 
@@ -15,8 +11,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("Prueba de modificación para git");
     }
     
 }
