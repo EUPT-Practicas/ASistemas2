29ad891026e76656d8b19e80ea6d403dfa851b17
Javi <jnavarroa90@gmail.com>
2015-03-17 21:56:55 +0100

    Practica2 funciona

--- Scripts_Python/listadoB.py
+++ Scripts_Python/listadoB.py
@@ -1,4 +1,4 @@
-./<'''
+'''
 @autores: Ricardo y Javier
 
 '''
@@ -6,34 +6,41 @@
 #!/usr/bin/python
 import sys, os, string, commands
 
-#COMANDO = "ls -la "
+def listar(directorio):
+	lista = []
+	print "|"+directorio+"|"
+	print "\n ----------------- "
+	print " Directorio: "+directorio
+	if os.path.isdir(directorio):
+		permisos = string.rsplit(commands.getoutput("ls -l " + directorio + " | grep -v 'total' | cut -d ' ' -f 1"),"\n") 		
+		archivos = string.rsplit(commands.getoutput("ls -1 " + directorio),"\n")
+		print " Permisos   Nombre "
+		print " ----------------- "
+		for i in range(len(permisos)):
+			print permisos[i] + " " + archivos[i]
+			if os.path.isdir(archivos[i]):
+				lista.append(archivos[i])
+	else:
+		print directorio + ": No es un directorio valido."
+	for i in range(len(lista)):
+		if comprob_permisos(lista[i]):
+			print lista[i]
+			listar(lista[i])
+
+def comprob_permisos(archivo):
+	status, out = commands.getstatusoutput("ls -l " + archivo)
+	if status > 0:
+		print "\nERROR: El directorio "+archivo+" no es accesible por el usuario.\n"
+		return False
+	else:
+		return True
+
+
+#Inicio programa
 
 if len(sys.argv) == 1:
 	directorio = raw_input("Introduzca el nombre del directorio: ")
-	if os.path.isdir(directorio):
-		permisos = string.rsplit(commands.getoutput("ls -lR " + directorio + " | grep -v 'total' | cut -d ' ' -f 1"),"\n") 		
-		archivos = string.rsplit(commands.getoutput("ls -1R " + directorio),"\n")
-		#comprobar = permisos[0]
-		print "Esto es:" + permisos[6] + "."
-		print " Permisos   Nombre"
-		for i in range(len(permisos)):
-			comprobar = permisos[i]		
-			if(comprobar != ""):
-				if(comprobar[0] == "." and  comprobar[1] == "/"):
-					print  permisos[i]
-					i += 1
-			print permisos[i] + " " + archivos[i]
-		
-	else:
-		print directorio + ": No es un directorio valido."	
-
+	if comprob_permisos(directorio):
+		listar(directorio)
 else:
 	print "ERROR: No debe de introducir ningun parametro"
-	
-
-
-
-
-
-	
-
